b8d1b00c35ac8190caf6f0917d70b5f4e221ad17
wulai‘s git <hello@itcast.cn>
2026-09-17 23:54:16 +0800

    漏传文件

---
+++ src/main/java/cn/edu/sdjzu/campussecondhandtradingsystem/vo/MessageItemVO.java
@@ -0,0 +1,86 @@
+package cn.edu.sdjzu.campussecondhandtradingsystem.vo;
+
+import java.time.LocalDateTime;
+
+public class MessageItemVO {
+
+    private Long id;
+
+    private Long productId;
+
+    private Long userId;
+
+    private String username;
+
+    private String content;
+
+    private Long parentId;
+
+    private String parentUsername;
+
+    private LocalDateTime createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getParentUsername() {
+        return parentUsername;
+    }
+
+    public void setParentUsername(String parentUsername) {
+        this.parentUsername = parentUsername;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+}
